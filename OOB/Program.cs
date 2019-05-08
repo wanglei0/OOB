f2becf86ac1cb4d44c8d4f1aa6b7dbd63b1d6a00
lei <lqwang@thoughtworks.com>
2019-05-08 18:22:27 +0800

    [zhiming&wanglei] create fizzbuz project

---
+++ OOB/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OOB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.Read();
+            
+            
+            
+            var result = FizzBuzz(input);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static bool ValidInput(string input)
+        {
+            try
+            {
+                Convert.ToInt32(input);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return true;
+        }
+
+        public static string FizzBuzz(int number)
+        {
+            if (number < 1 || number > 100)
+                return "invalid";
+
+            else if(number % 3 == 0 && number % 5 != 0)
+            {
+                return "Fizz";
+            }
+            else if(number % 5 == 0 && number % 3 != 0)
+            {
+                return "Buzz";
+            }
+            else if(number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else 
+                return number.ToString();
+            
+        }
+    }
+}
